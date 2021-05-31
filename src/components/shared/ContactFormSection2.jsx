252d410e1f10e43347913e29afd3f697d55b0d80
rupalmalik3 <rupalmalik3@gmail.com>
2021-05-31 13:58:35 +0530

    fix : centered the contact form

--- src/components/shared/ContactFormSection2.jsx
+++ src/components/shared/ContactFormSection2.jsx
@@ -11,7 +11,7 @@ export default class ContactFormSection2 extends PureComponent {
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
+          <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">
